48c7420a4683839aff69fc9a2030a541c0f8bbe5
budarov <budarov@haulmont.com>
2023-11-17 16:45:58 +0400

    project created

---
+++ src/main/java/com/sample/doctorsdemo/DoctorsdemoApplication.java
@@ -0,0 +1,14 @@
+package com.sample.doctorsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class DoctorsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorsdemoApplication.class, args);
+    }
+}
